04e1103fc28413fa2ab0438d0973a054eece4753
Shabad-Chahal <shabadchahalc@gmail.com>
2025-09-22 22:16:38 +0530

    change
    change

--- static/js/student-record.js
+++ static/js/student-record.js
@@ -52,9 +52,4 @@ students.forEach(s=>{
   row.insertCell(6).innerText = s.performance;
 });
 
- const menuToggle = document.getElementById('menuToggle');
-const sidebar = document.getElementById('sidebar');
-
-menuToggle.addEventListener('click', () => {
-  sidebar.classList.toggle('collapsed');
-});
+ 
