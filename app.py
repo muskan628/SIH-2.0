7b8bddcc96d983f011397f0836f2e8ddbcba1e7b
nitin-1212 <133770229+nitin-1212@users.noreply.github.com>
2025-10-10 19:43:42 +0530

    hi

--- app.py
+++ app.py
@@ -114,6 +114,14 @@ class StudentRegistration(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)    
 
+class StudentRegistration(db.Model):
+    __tablename__ = "registration_students"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)    
+
     def __repr__(self):
         return f'<User {self.username}>'
 
